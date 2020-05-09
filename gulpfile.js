43e0ef5262ee0b9465a15eba92412b09c90f0e8c
GabrielLinss <glins731@gmail.com>
2020-05-09 18:52:32 -0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
+                .pipe(sass())
+                .pipe(gulp.dest('src/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series('sass', 'watch'));
